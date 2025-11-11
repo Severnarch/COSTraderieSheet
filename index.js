f1eb7d55538419c4198cb3c2486d09cf5c25ab8f
Severnarch <86313252+Severnarch@users.noreply.github.com>
2025-11-11 22:21:53 +0000

    list making

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const sheetsUrl = "sheets/";
 
 async function main() {
 	// Setup data
-	var itemsSheet = {};
+	var itemsSheet = [];
 
+	// Fetch data
 	try {
 		const response = await fetch(sheetsUrl+"items.csv");
 		if (!response.ok) {
@@ -12,10 +13,27 @@ async function main() {
 		}
 
 		const result = await response.text();
-		console.log(result)
+		if (result) {
+			result.then(raw=>{
+				rows = raw.split("\r\n")
+				headers = rows[0].split(",")
+				for (i = 1; i < rows.length; i++) {
+					if (rows[i].length > 0) {
+						const robj = rows[i].split(",")
+						var obj = {}
+						for (j = 0; j < robj.length; j++) {
+							obj[headers[j]] = robj[j]
+						}
+						itemsSheet.append(obj)
+					}
+				}
+			});
+		}
 	} catch (error) {
 		throw new Error("Error fetching data:", error)
 	}
+
+	console.log()
 }
 
 window.addEventListener('load', main);
