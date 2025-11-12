dbbbd3b04c55367aef0f7f58a5465bf11af4d349
Severnarch <86313252+Severnarch@users.noreply.github.com>
2025-11-12 17:46:53 +0000

    wah

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 // Constants
 const sheetsUrl = "sheets/";
 
+function camelToTitle(val) {
+	val = val.replace(/([A-Z])/g, " $1");
+	val = val.charAt(0).toUpperCase() + val.slice(1)
+	return val;
+}
+function formatString(val) {
+	val = val.replace(/\w[a-z]*/g, function(x){return x.charAt(0).toUpperCase()+x.substr(1).toLowerCase();});
+	val = val.charAt(0).toUpperCase() + val.slice(1)
+	return val;
+}
+
 async function main() {
 	// Setup data
 	var itemsSheet = [];
@@ -9,29 +20,54 @@ async function main() {
 	try {
 		const response = await fetch(sheetsUrl+"items.csv");
 		if (!response.ok) {
-			throw new Error(`Response status: ${response.status}`)
+			console.log(`Fetching CSV file returned ${response.status}`);
 		}
 
 		const result = await response.text();
 		if (result) {
-			rows = result.split("\r\n")
-			headers = rows[0].split(",")
+			rows = result.split("\r\n");
+			headers = rows[0].split(",");
 			for (i = 1; i < rows.length; i++) {
 				if (rows[i].length > 0) {
-					const robj = rows[i].split(",")
-					var obj = {}
+					const robj = rows[i].split(",");
+					var obj = {};
 					for (j = 0; j < robj.length; j++) {
-						obj[headers[j]] = robj[j]
+						obj[headers[j]] = robj[j];
 					}
-					itemsSheet.append(obj)
+					itemsSheet.append(obj);
 				}
 			}
 		}
 	} catch (error) {
-		throw new Error("Error fetching data:", error)
+		console.log(error);
 	}
 
-	console.log()
+	const tableHolder = document.getElementById("table-holder");
+	if (itemsSheet.length > 0) {
+		var tableElement = document.createElement("table");
+
+		var headerRow = document.createElement("tr");
+		for (i in headers) {
+			var headerCell = document.createElement("th");
+			headerCell.innerText = camelToTitle(headers[i]);
+			headerRow.appendChild(headerCell);
+		}
+		tableElement.appendChild(headerRow);
+
+		for (i in itemsSheet) {
+			var itemRow = document.createElement("tr");
+
+			for (j in headers) {
+				var itemCell = document.createElement("td");
+				itemCell.innerText = formatString(itemsSheet[i][headers[j]])
+				itemRow.appendChild(itemCell);
+			}
+
+			tableElement.appendChild(itemRow);
+		}
+
+		tableHolder.appendChild(tableElement);
+	}
 }
 
 window.addEventListener('load', main);
